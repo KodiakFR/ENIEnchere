c8c8c791e8c9b16b543cc45019fb521763f7ac50
Maxime Braud <maxime.braud86@gmail.com>
2021-06-15 14:38:00 +0200

    création du fichier UtilisateurManager

---
+++ src/fr/eni/encheres/bll/UtilisateurManager.java
@@ -0,0 +1,10 @@
+package fr.eni.encheres.bll;
+
+public class UtilisateurManager {
+	
+	//Attributes
+	
+	private UtilisateurDAO daoUtilisateur;
+	private static UtilisateurManager instance = null;
+
+}
