8d591e4fcce1c2c74774361a5034fec9aa8d7cc8
RomainBEGUE <romain.begue@live.fr>
2021-06-15 09:05:17 +0200

    BO articlevendu & DAL ConnectionProvider

---
+++ src/fr/eni/encheres/bo/ArticleVendu.java
@@ -0,0 +1,154 @@
+package fr.eni.encheres.bo;
+
+import java.util.Date;
+
+public class ArticleVendu {
+	private int noArticle;
+	private String nomArticle;
+	private String description;
+	private Date dateDebutEncheres;
+	private Date dateFinEncheres;
+	private int miseAPrix;
+	private int prixVente;
+	private int etatVente;
+	
+	
+	public ArticleVendu() {
+		super();
+	}
+
+
+	public ArticleVendu(String nomArticle, String description, Date dateDebutEncheres, Date dateFinEncheres,
+			int miseAPrix, int prixVente, int etatVente) {
+		super();
+		this.nomArticle = nomArticle;
+		this.description = description;
+		this.dateDebutEncheres = dateDebutEncheres;
+		this.dateFinEncheres = dateFinEncheres;
+		this.miseAPrix = miseAPrix;
+		this.prixVente = prixVente;
+		this.etatVente = etatVente;
+	}
+
+
+	public ArticleVendu(int noArticle, String nomArticle, String description, Date dateDebutEncheres,
+			Date dateFinEncheres, int miseAPrix, int prixVente, int etatVente) {
+		super();
+		this.noArticle = noArticle;
+		this.nomArticle = nomArticle;
+		this.description = description;
+		this.dateDebutEncheres = dateDebutEncheres;
+		this.dateFinEncheres = dateFinEncheres;
+		this.miseAPrix = miseAPrix;
+		this.prixVente = prixVente;
+		this.etatVente = etatVente;
+	}
+
+
+	public int getNoArticle() {
+		return noArticle;
+	}
+
+
+	public void setNoArticle(int noArticle) {
+		this.noArticle = noArticle;
+	}
+
+
+	public String getNomArticle() {
+		return nomArticle;
+	}
+
+
+	public void setNomArticle(String nomArticle) {
+		this.nomArticle = nomArticle;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public Date getDateDebutEncheres() {
+		return dateDebutEncheres;
+	}
+
+
+	public void setDateDebutEncheres(Date dateDebutEncheres) {
+		this.dateDebutEncheres = dateDebutEncheres;
+	}
+
+
+	public Date getDateFinEncheres() {
+		return dateFinEncheres;
+	}
+
+
+	public void setDateFinEncheres(Date dateFinEncheres) {
+		this.dateFinEncheres = dateFinEncheres;
+	}
+
+
+	public int getMiseAPrix() {
+		return miseAPrix;
+	}
+
+
+	public void setMiseAPrix(int miseAPrix) {
+		this.miseAPrix = miseAPrix;
+	}
+
+
+	public int getPrixVente() {
+		return prixVente;
+	}
+
+
+	public void setPrixVente(int prixVente) {
+		this.prixVente = prixVente;
+	}
+
+
+	public int getEtatVente() {
+		return etatVente;
+	}
+
+
+	public void setEtatVente(int etatVente) {
+		this.etatVente = etatVente;
+	}
+
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("ArticleVendu [noArticle=");
+		builder.append(noArticle);
+		builder.append(", nomArticle=");
+		builder.append(nomArticle);
+		builder.append(", description=");
+		builder.append(description);
+		builder.append(", dateDebutEncheres=");
+		builder.append(dateDebutEncheres);
+		builder.append(", dateFinEncheres=");
+		builder.append(dateFinEncheres);
+		builder.append(", miseAPrix=");
+		builder.append(miseAPrix);
+		builder.append(", prixVente=");
+		builder.append(prixVente);
+		builder.append(", etatVente=");
+		builder.append(etatVente);
+		builder.append("]");
+		return builder.toString();
+	}
+
+
+
+	
+}
