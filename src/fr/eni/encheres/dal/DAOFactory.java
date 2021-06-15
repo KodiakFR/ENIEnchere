2b01ac93b4b2e2fb02ebd3b8d5f63f59c5aea561
Maxime Braud <maxime.braud86@gmail.com>
2021-06-15 14:40:43 +0200

    Ajout de l'instance utilisateur dans la Factory

--- src/fr/eni/encheres/dal/DAOFactory.java
+++ src/fr/eni/encheres/dal/DAOFactory.java
@@ -5,4 +5,8 @@ public class DAOFactory {
 	public static ArticleVenduDAOImpl getArticleVenduDAO() {
 		return new ArticleVenduDAOImpl();
 	}
+	
+	public static UtilisateurDAOImpl getUtilisateurDAO() {
+		return new UtilisateurDAOImpl();
+	}
 }
