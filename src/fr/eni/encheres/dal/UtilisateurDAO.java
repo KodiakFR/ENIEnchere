9797664f9ea5422cd10fe4b2eed6a83e8f17bb06
Dimitri <dig72000@gmail.com>
2021-06-15 16:38:01 +0200

    MAJ util dao des methodes inscription

--- src/fr/eni/encheres/dal/UtilisateurDAO.java
+++ src/fr/eni/encheres/dal/UtilisateurDAO.java
@@ -1,5 +1,7 @@
 package fr.eni.encheres.dal;
 
+import java.util.List;
+
 import fr.eni.encheres.bll.BusinessException;
 import fr.eni.encheres.bo.Utilisateur;
 
@@ -8,5 +10,12 @@ public interface UtilisateurDAO {
 	
 	// Insertion de la méthode insert inscription // Dimitri
 	public void insertInscription(Utilisateur utili) throws BusinessException;
+	
+	
+	// Selection de la liste des pseudos pour vérifier // Dimitri
+	public List<String> validationPseudo() throws BusinessException;
+	
+	
+	
 	
 }
