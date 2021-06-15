9c90c2f74b3b1827b2af86e2a14d7b561546b39c
Dimitri <dig72000@gmail.com>
2021-06-15 17:51:20 +0200

    modif utilisateur dao fin dim

--- src/fr/eni/encheres/dal/UtilisateurDAO.java
+++ src/fr/eni/encheres/dal/UtilisateurDAO.java
@@ -11,11 +11,11 @@ public interface UtilisateurDAO {
 	// Insertion de la méthode insert inscription // Dimitri
 	public void insertInscription(Utilisateur utili) throws BusinessException;
 	
-	
 	// Selection de la liste des pseudos pour vérifier // Dimitri
 	public List<String> validationPseudo() throws BusinessException;
 	
-	
+	// Methode de verif unicite email
+	public List<String> validationEmail() throws BusinessException;
 	
 	
 }
