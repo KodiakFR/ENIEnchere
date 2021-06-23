96465756e2a060f78c2ee47c04bdceab1f06d85e
Dimitri <dig72000@gmail.com>
2021-06-23 12:23:24 +0200

    correction servlet en cours

--- src/fr/eni/encheres/servlets/ServletProfil.java
+++ src/fr/eni/encheres/servlets/ServletProfil.java
@@ -36,13 +36,13 @@ public class ServletProfil extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-					
+		String utilisateurGeneral = (String) request.getSession().getAttribute("Utilisateur");		
+		
 		if (request.getServletPath().equals("/Profil")) {
 			
 			try {
 				boolean valideP = false;
-				UtilisateurManager utilisateur = UtilisateurManager.getInstance();
-				String utilisateurGeneral = (String) request.getSession().getAttribute("Utilisateur");
+				UtilisateurManager utilisateurManager = UtilisateurManager.getInstance();
 				System.out.println("utilisateurGeneral= " + utilisateurGeneral);
 	
 				
@@ -51,7 +51,7 @@ public class ServletProfil extends HttpServlet {
 				String pseudoRecup = request.getParameter("pseudo");
 				
 				System.out.println("je suis dans le if 1er");	
-				Utilisateur utilisateurInconnu = utilisateur.recuperationUtilisateur(utilisateurGeneral);
+				Utilisateur utilisateurInconnu = utilisateurManager.recuperationUtilisateur(utilisateurGeneral);
 				request.setAttribute("utilisateurInconnu", utilisateurInconnu);
 				
 				if(pseudoRecup.equals(utilisateurGeneral)) {
@@ -75,10 +75,9 @@ public class ServletProfil extends HttpServlet {
 		if (request.getServletPath().equals("/ModifProfil")) {
 			
 			try {
-				UtilisateurManager utilisateur = UtilisateurManager.getInstance();
-				String utilisateurGener = (String) request.getSession().getAttribute("Utilisateur");
-				Utilisateur utilisateurProfil = utilisateur.recuperationUtilisateur(utilisateurGener);
-				System.out.println("pseudo sauvegardé c'est bon " + utilisateurGener);
+				UtilisateurManager utilisateurManager = UtilisateurManager.getInstance();
+				Utilisateur utilisateurProfil = utilisateurManager.recuperationUtilisateur(utilisateurGeneral);
+				System.out.println("pseudo sauvegardé c'est bon " + utilisateurGeneral);
 
 				request.setAttribute("utilisateurProfil", utilisateurProfil);
 				RequestDispatcher rd  = request.getRequestDispatcher("/WEB-INF/JSP/ModificationProfil.jsp");
@@ -94,9 +93,12 @@ public class ServletProfil extends HttpServlet {
 						
 				try {
 					UtilisateurManager utilisateurManager = UtilisateurManager.getInstance();
-					String utilisateurSession = (String) request.getSession().getAttribute("Utilisateur");
-					Utilisateur utilisateurSuppr = utilisateurManager.suppressionProfil(utilisateurSession);
-				} catch (Exception e) {
+					utilisateurManager.suppressionProfil(utilisateurGeneral);
+					request.getSession().invalidate();
+					RequestDispatcher rd  = request.getRequestDispatcher("/WEB-INF/JSP/Accueil.jsp");
+					rd.forward(request, response);
+					
+				} catch (BusinessException e) {
 					// TODO: handle exception
 				}	
 		}
@@ -115,8 +117,7 @@ public class ServletProfil extends HttpServlet {
 		
 		
 		try {
-			
-		
+
 			UtilisateurManager utilisateurManager = UtilisateurManager.getInstance();
 			String utilisateurGener = (String) request.getSession().getAttribute("Utilisateur");
 			Utilisateur utilisateurProfil = utilisateurManager.recuperationUtilisateur(utilisateurGener);
@@ -138,40 +139,51 @@ public class ServletProfil extends HttpServlet {
 			verifMdp = utilisateurManager.validerMDP(mdpAc);
 			System.out.println(verifMdp + " reponse");
 			
+			String mailUtil = utilisateurManager.recuperationUtilisateur(utilisateurGener).getEmail();
 			//if verifie si mdp bon
-			if(verifMdp == true) {
-				verifMail = utilisateurManager.validerMailModifProfil(email);
-				verifPseudo = utilisateurManager.validerPseudoModifProfil(pseudo);
-				System.out.println(verifMail);
-				System.out.println(verifPseudo);
-				// Methode verifie que mail et pseudo ne sont pas déjà existant
-				if(verifMail == false && verifPseudo == false) {
-					//méthode verifie si le nouveau mdp ne sont pas : null
-					if(newMdp.length()>0 & confirmMdp.length()>0) {
-						//methode verifie si les deux nouveaux mdp sont egaux
-						if(newMdp.equals(confirmMdp)) {
-							Utilisateur utilisateur = new Utilisateur(pseudo, nom, prenom, email, tel, rue, cp, ville, newMdp);
-							utilisateurManager.modificationProfil(utilisateur);
-							System.out.println("je passe 1");
-						}
-					}
-					// Vérifie si les deux new mdps sont : null
-					if(newMdp.length()==0 & confirmMdp.length()==0) {
-						Utilisateur utilisateur = new Utilisateur(pseudo, nom, prenom, email, tel, rue, cp, ville, mdpAc);
-						utilisateurManager.modificationProfil(utilisateur);	
-						System.out.println("je passe 2");
-					}
+ 			if(verifMdp == true) {
+ 				
+				verifMail = utilisateurManager.validerMail(email);
+				verifPseudo = utilisateurManager.validerPseudo(pseudo);
+				
+				if(verifMail == false || email.equals(mailUtil) && verifPseudo == false || pseudo.equals(utilisateurGener)) {
+				System.out.println("le mail verifié retourne = " +verifMail);
+				System.out.println("le pseudo verifié retourne = " +verifPseudo);
+				System.out.println("le mail utilisateur est " + mailUtil);
+				System.out.println("le pseudo utilisateur est " +utilisateurGener);
+				System.out.println(email);
+				System.out.println(pseudo);
+				
+				// Methode verifie que mail et pseudo ne sont pas dÃ©jÃ  existant
+				//if(verifMail == false && verifPseudo == false) {
+					
+					//mÃ©thode verifie si le nouveau mdp ne sont pas : null
+					//methode verifie si les deux nouveaux mdp sont egaux
+ 					if(newMdp.length()>0 && confirmMdp.length()>0 && newMdp.equals(confirmMdp)) {
+ 					
+ 							Utilisateur utilisateur = new Utilisateur(pseudo, nom, prenom, email, tel, rue, cp, ville, newMdp);
+ 							utilisateurManager.modificationProfil(utilisateur);
+ 							System.out.println("je passe 1");
+ 						
+ 					}
+					// VÃ©rifie si les deux new mdps sont : null
+ 					if(newMdp.length()==0 && confirmMdp.length()==0) {
+ 						Utilisateur utilisateur = new Utilisateur(pseudo, nom, prenom, email, tel, rue, cp, ville, mdpAc);
+ 						utilisateurManager.modificationProfil(utilisateur);	
+ 						System.out.println("je passe 2");
+ 					}
 					// si mdp les deux mdp ne sont pas egaux
-					else if(!newMdp.equals(confirmMdp)) {
-						validationMdp = true;
-						request.setAttribute("validationMdp", validationMdp);
+ 					else if(!newMdp.equals(confirmMdp)) {
+ 						validationMdp = true;
+ 						request.setAttribute("validationMdp", validationMdp);
 						request.setAttribute("utilisateurProfil", utilisateurProfil);
-						RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/JSP/ModificationProfil.jsp") ;
-						rd.forward(request, response);
-						System.out.println("je passe 3");
-					}
+ 						RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/JSP/ModificationProfil.jsp") ;
+ 						rd.forward(request, response);
+ 						System.out.println("je passe 3");
+ 					}
+	
 				}
-				//si mail et pseudo existe déjà renvoie message
+				//si mail et pseudo existe dÃ©jÃ  renvoie message
 				else if(verifMail == true || verifPseudo == true) {
 					verifMail = true;
 					request.setAttribute("verifMail", verifMail);
@@ -179,18 +191,18 @@ public class ServletProfil extends HttpServlet {
 					RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/JSP/ModificationProfil.jsp") ;
 					rd.forward(request, response);
 				}
-			}
-			
+ 			}
+ 			
+			// methode verif si les deux nouveaux mdp saisie sont egaux
 			// methode envoie erreur si le mdp saisie n'est pas bon  pas egaux
-			 if(verifMdp == false) {
-				validationMdpAc = true;
+ 			 if(verifMdp == false) {
+ 				validationMdpAc = true;
 				request.setAttribute("validationMdpAc", validationMdpAc);
 				request.setAttribute("utilisateurProfil", utilisateurProfil);
 				RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/JSP/ModificationProfil.jsp") ;
 				rd.forward(request, response);
-				System.out.println("je passe 4");
-			}
-			
+				System.out.println("je passe 4");	
+ 			 }
 			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/JSP/Accueil.jsp") ;
 			rd.forward(request, response);
 			
